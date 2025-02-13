c42b095dff209edcd4979f0ae4a52434933ca822
hawri <hawri@DESKTOP-BMM1386>
2025-02-13 11:52:58 +0300

    Schedule in progres

--- T-SQL/SQLQuery1 - Schedule.sql
+++ T-SQL/SQLQuery1 - Schedule.sql
@@ -3,14 +3,14 @@ GO
 
 SET DATEFIRST 1;
 
-DECLARE @group				AS INT			= (SELECT group_id			FROM Groups			WHERE group_name = N'PV_319');
-DECLARE @discipline			AS SMALLINT		= (SELECT discipline_id		FROM Disciplines	WHERE discipline_name LIKE N'Объектно-ориентированное программирование%');
-DECLARE @teacher			AS SMALLINT		= (SELECT teacher_id		FROM Teachers		WHERE first_name = N'Олег');
-DECLARE @start_date			AS DATE			= N'2024-06-01';
-DECLARE @date				AS DATE			= @start_date;
-DECLARE @number_of_lessons	AS TINYINT		= (SELECT number_of_lessons FROM Disciplines	WHERE discipline_id = @discipline);
-DECLARE @lesson				AS TINYINT		= 1;
-DECLARE @time				AS TIME(0)		= N'18:30';
+DECLARE @group					AS INT			= (SELECT group_id			FROM Groups			WHERE group_name = N'PV_319');
+DECLARE @discipline				AS SMALLINT		= (SELECT discipline_id		FROM Disciplines	WHERE discipline_name LIKE N'Объектно-ориентированное программирование%');
+DECLARE @teacher				AS SMALLINT		= (SELECT teacher_id		FROM Teachers		WHERE first_name = N'Олег');
+DECLARE @start_date				AS DATE			= N'2024-06-01';
+DECLARE @date					AS DATE			= @start_date;
+DECLARE @number_of_lessons		AS TINYINT		= (SELECT number_of_lessons FROM Disciplines	WHERE discipline_id = @discipline);
+DECLARE @lesson					AS TINYINT		= 1;
+DECLARE @time					AS TIME(0)		= N'18:30';
 
 WHILE(@lesson <= @number_of_lessons)
 BEGIN
