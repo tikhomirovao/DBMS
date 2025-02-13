6ee3727dc4e0e67d5a9e48821ce7cf158f0c5f27
hawri <hawri@DESKTOP-BMM1386>
2025-02-13 16:26:28 +0300

    HW

--- T-SQL/SQLQuery1 - INSERT PV_319.sql
+++ T-SQL/SQLQuery1 - INSERT PV_319.sql
@@ -5,4 +5,4 @@ INSERT Groups
 			(group_id, group_name, direction)
 VALUES		(319,		N'PV_319',	1);
 
-SELECT * FROM Groups;
+SELECT * FROM Teachers;
--- T-SQL/SQLQuery1 - Schedule.sql
+++ T-SQL/SQLQuery1 - Schedule.sql
@@ -1,26 +1,45 @@
-п»їUSE	PV_319_Import;
+USE	PV_319_Import;
 GO
 
 SET DATEFIRST 1;
 
-DECLARE @group					AS INT			= (SELECT group_id			FROM Groups			WHERE group_name = N'PV_319');
-DECLARE @discipline				AS SMALLINT		= (SELECT discipline_id		FROM Disciplines	WHERE discipline_name LIKE N'РћР±СЉРµРєС‚РЅРѕ-РѕСЂРёРµРЅС‚РёСЂРѕРІР°РЅРЅРѕРµ РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёРµ%');
-DECLARE @teacher				AS SMALLINT		= (SELECT teacher_id		FROM Teachers		WHERE first_name = N'РћР»РµРі');
-DECLARE @start_date				AS DATE			= N'2024-06-01';
-DECLARE @date					AS DATE			= @start_date;
-DECLARE @number_of_lessons		AS TINYINT		= (SELECT number_of_lessons FROM Disciplines	WHERE discipline_id = @discipline);
-DECLARE @lesson					AS TINYINT		= 1;
-DECLARE @time					AS TIME(0)		= N'18:30';
+DECLARE @group					AS INT				= (SELECT group_id			FROM Groups					WHERE group_name = N'PV_319');
+DECLARE @group_name				AS NVARCHAR(150)	= (SELECT group_name		FROM Groups,Schedule		WHERE group_id = @group);
+DECLARE @discipline				AS SMALLINT			= (SELECT discipline_id		FROM Disciplines			WHERE discipline_name LIKE N'Объектно-ориентированное программирование%');
+DECLARE @discipline_name		AS NVARCHAR(150)	= (SELECT discipline_name	FROM Disciplines,Schedule	WHERE discipline_id = @discipline);
+DECLARE @teacher				AS SMALLINT			= (SELECT teacher_id		FROM Teachers				WHERE first_name = N'Олег');
+DECLARE @teacher_name			AS NVARCHAR(150)	= (SELECT first_name		FROM Teachers,Schedule		WHERE teacher_id = @teacher); 
+DECLARE @start_date				AS DATE				= N'2024-06-01';
+DECLARE @date					AS DATE				= @start_date;
+DECLARE @number_of_lessons		AS TINYINT			= (SELECT number_of_lessons FROM Disciplines			WHERE discipline_id = @discipline);
+DECLARE @lesson					AS TINYINT			= 1;
+DECLARE @time					AS TIME(0)			= N'18:30';
+--DECLARE @date_now				AS DATE				= N'2024-06-08'
 
 WHILE(@lesson <= @number_of_lessons)
 BEGIN
-		PRINT(@date);
+		PRINT(@group_name);
+		PRINT(@discipline_name);
 		PRINT(DATENAME(WEEKDAY, @date));
+		PRINT(@date);
 		PRINT(@lesson);
 		PRINT(@time);
 		SET @lesson = @lesson + 1;
 		PRINT(@lesson);
 		PRINT(DATEADD(MINUTE, 95, @time));
+		PRINT(@teacher_name);
+		IF(@date < GETDATE())
+		BEGIN
+			PRINT N'Проведено'
+		END
+		IF(@date > GETDATE())
+		BEGIN
+			PRINT N'Запланировано'
+		END
+		IF(@date = GETDATE())
+		BEGIN
+			PRINT N'Занятие сегодня'
+		END
 		PRINT('-------------------------------');
 		IF(DATEPART(WEEKDAY, @date) = 6)
 		BEGIN
@@ -31,7 +50,6 @@ BEGIN
 			SET @date = DATEADD(DAY, 2, @date);
 		END
 END
-
 --INSERT Schedule
 --			([group], discipline, teacher, [date], [time], spent)
 --VALUES
