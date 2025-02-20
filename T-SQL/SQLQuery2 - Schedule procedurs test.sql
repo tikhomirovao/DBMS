9ea12803fab1c9ed31b78cf5a2bbf0f2d6bb564a
hawri <hawri@DESKTOP-BMM1386>
2025-02-20 16:08:23 +0300

    Stored procedures day 2

--- T-SQL/SQLQuery2 - Schedule procedurs test.sql
+++ T-SQL/SQLQuery2 - Schedule procedurs test.sql
@@ -1,7 +1,13 @@
-﻿USE PV_319_Import
+﻿USE PV_319_Import;
 GO
 
-EXEC sp_ScheduleForStacionarGroup N'PV_319', N'%UML%', N'Ковтун', N'2024-08-10', N'18:30';
+--EXEC sp_SetScheduleForStacionarGroup N'PV_319', N'%UML%', N'Ковтун', N'2024-08-10', N'18:30';
+EXEC sp_ScheduleForStacionarGroup N'PD_321', N'%MS SQL Server%', N'Покидюк', N'2024-06-01', N'13:30';
 
-EXEC sp_PrintScheduleForGroup N'PV_319';
+EXEC sp_PrintScheduleForGroup N'PD_321';
+--EXEC sp_PrintScheduleForGroupPeriod N'PV_319', '2024-06-01', '2024-07-01';
+
+DECLARE @salary SMALLMONEY;
+EXEC @salary = sp_CountTeachersMonthSalary N'Ковтун', 2024, 06;
+PRINT (@salary);
 --SELECT * FROM Schedule;
