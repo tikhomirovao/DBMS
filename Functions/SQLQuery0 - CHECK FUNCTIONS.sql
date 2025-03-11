d7cdd0b323c3979773878a938aeb54233f5ed0fd
hawri <hawri@DESKTOP-BMM1386>
2025-03-11 14:06:53 +0300

    GetEasterDate DONE

--- Functions/SQLQuery0 - CHECK FUNCTIONS.sql
+++ Functions/SQLQuery0 - CHECK FUNCTIONS.sql
@@ -2,13 +2,14 @@ USE PV_319_Import;
 SET DATEFIRST 1;
 GO
 
-PRINT(dbo.GetGroupID(N'PV_319'));
-PRINT(dbo.GetDisciplineID(N'Hardware%'));
---EXEC sp_PrintScheduleForGroup N'PV_319';
-PRINT(dbo.CountSpentLessons(N'PV_319', N'Процедурное программирование%'));
-PRINT(dbo.GetNewYearHolidaysStartDate(2025));
-PRINT(dbo.GetSummerHolidaysStart(2025));
-PRINT(dbo.GetHolidaysDuration(N'Летние%'));
-
-PRINT('----------------------------------')
-PRINT(dbo.GetHolidaysStartDate(N'8%', 2025));
+--PRINT(dbo.GetGroupID(N'PV_319'));
+--PRINT(dbo.GetDisciplineID(N'Hardware%'));
+----EXEC sp_PrintScheduleForGroup N'PV_319';
+--PRINT(dbo.CountSpentLessons(N'PV_319', N'Процедурное программирование%'));
+--PRINT(dbo.GetNewYearHolidaysStartDate(2025));
+--PRINT(dbo.GetSummerHolidaysStart(2025));
+--PRINT(dbo.GetHolidaysDuration(N'Летние%'));
+--
+--PRINT('----------------------------------')
+--PRINT(dbo.GetHolidaysStartDate(N'8%', 2025));
+PRINT(dbo.GetEasterDate(2025));
---
+++ Functions/SQLQuery7 - GetEasterDate.sql
@@ -0,0 +1,13 @@
+USE PV_319_Import;
+GO
+
+CREATE FUNCTION GetEasterDate(@year SMALLINT)RETURNS DATE
+BEGIN
+	DECLARE @a	AS	SMALLINT = @year%19;
+	DECLARE @b	AS	SMALLINT = @year%4;
+	DECLARE @c	AS	SMALLINT = @year%7;
+	DECLARE @d	AS	SMALLINT = (19*@a + 15)%30;
+	DECLARE @e	AS	SMALLINT = (2*@b + 4*@c + 6*@d + 6) % 7;
+	DECLARE @f	AS	SMALLINT = @d + @e;
+	RETURN	IIF(@f <= 26, DATEFROMPARTS(@year, 4, 4+@f), DATEFROMPARTS(@year, 5, @f-26));
+END
